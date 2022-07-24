a8bc84cf9729aa36344c1eb44c005e30ad5c9b92
Brij Singla <brij.singla@zs.com>
2022-07-24 14:40:46 +0530

    Pie Chart with Dummy data

--- graph.js
+++ graph.js
@@ -1,29 +1,57 @@
-const container =d3.select('.graph-container')
-const svg=container.append('svg')
-        .attr('height',500)
-        .attr('width',600)
+let dims = { height: 400, width: 400, radius: 150 };
 
-let render=(values=>{
+const container = d3.select('.graph-container')
+const svg = container.append('svg')
+        .attr('height', dims.height+150)
+        .attr('width', dims.width+150)
+
+const graph = svg.append('g')
+        .attr('height', dims.height)
+        .attr('width', dims.width)
+        .attr('transform',`translate(${(dims.width/2)+75},${(dims.height/2) + 75 })`);
+
+const pie = d3.pie()
+        .sort(null)
+        .value(d => d);
+
+const arc = d3
+        .arc()
+        .outerRadius(dims.radius)
+        .innerRadius(0);
+
+graph.selectAll('path').data(pie([1, 2,3,4]))
+        .enter()
+        .append('path')
+        .attr('d', d => arc(d))
+        .attr('fill', (d, i) => (i % 2 == 0) ? 'orange' : 'red');
+
+
+
+
+
+
+
+let render = (values => {
     console.log(values);
 })
-let values=[];
+let values = [];
 
-db.collection('Investments').onSnapshot((res)=>{
+db.collection('Investments').onSnapshot((res) => {
     res.docChanges().forEach((change) => {
-        let doc= {...change.doc.data(),id:change.doc.id};
+        let doc = { ...change.doc.data(), id: change.doc.id };
 
-        if(change.type=='added'){
+        if (change.type == 'added') {
             values.push(doc);
-        }else if(change.type=='modified'){
-            values.map(item=>{
-                if(item.id===doc.id){
-                    item.cost=doc.cost;
+        } else if (change.type == 'modified') {
+            values.map(item => {
+                if (item.id === doc.id) {
+                    item.cost = doc.cost;
                 }
             });
         }
-        else if(change.type=="removed"){
-            values=values.filter(item=>item.id!==doc.id);
+        else if (change.type == "removed") {
+            values = values.filter(item => item.id !== doc.id);
         }
     });
-    render(values);
+    // render(values);
 })
